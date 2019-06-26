6ed70694a6ecfd2afe51ec04118e6a7e73cca76f
7mioda <ahmededaly1993@gmail.com>
2019-06-26 20:40:19 +0200

    fixing logger

--- dist/utils/logger.js
+++ dist/utils/logger.js
@@ -5,10 +5,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.log = undefined;
 
-var _logUpdate = require("log-update");
-
-var _logUpdate2 = _interopRequireDefault(_logUpdate);
-
 var _chalk = require("chalk");
 
 var _chalk2 = _interopRequireDefault(_chalk);
@@ -16,5 +12,5 @@ var _chalk2 = _interopRequireDefault(_chalk);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var log = exports.log = function log(string) {
-  (0, _logUpdate2["default"])(_chalk2["default"].blue.bgRed.bold(string));
+  console.log(_chalk2["default"].blue.bgRed.bold(string));
 };
