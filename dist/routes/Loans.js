81308a0b3d914bbad1a19862e65b938c0569fe50
7mioda <ahmededaly1993@gmail.com>
2019-06-26 21:09:53 +0200

    build process

--- dist/routes/Loans.js
+++ dist/routes/Loans.js
@@ -8,15 +8,15 @@ var _express = require("express");
 
 var _express2 = _interopRequireDefault(_express);
 
-var _loans = require("../controllers/loans");
+var _Loans = require("../controllers/Loans");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var router = _express2["default"].Router();
 
-router.get('/', _loans.getAllLoans);
-router.get('/:loanId', _loans.getLoan);
-router.post('/add', _loans.addLoan);
-router.post('/update', _loans.updateLoan);
-router["delete"]('/delete/:loanId', _loans.deleteLoan);
+router.get('/', _Loans.getAllLoans);
+router.get('/:loanId', _Loans.getLoan);
+router.post('/add', _Loans.addLoan);
+router.post('/update', _Loans.updateLoan);
+router["delete"]('/delete/:loanId', _Loans.deleteLoan);
 exports["default"] = router;
